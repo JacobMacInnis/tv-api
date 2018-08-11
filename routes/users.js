6edf29023b93ec0b5b85680ad6f95c270c7a034f
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-08-11 13:27:12 -0400

    completed project

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('index', {title: 'WHATEVER'});
 });
 
 module.exports = router;
